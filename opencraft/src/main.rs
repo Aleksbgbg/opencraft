659b101694b484f66a2125abe8184b29965b2bef
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-14 11:51:09 +0100

    Open a window using winit

--- opencraft/src/main.rs
+++ opencraft/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-  println!("Hello, world!");
+use anyhow::Result;
+use winit::event::{ElementState, Event, KeyEvent, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::keyboard::{KeyCode, PhysicalKey};
+use winit::window::WindowBuilder;
+
+fn main() -> Result<()> {
+  let event_loop = EventLoop::new()?;
+  let _window = WindowBuilder::new()
+    .with_title("Opencraft")
+    .build(&event_loop)?;
+
+  #[allow(clippy::single_match)]
+  event_loop.run(|event, target| match event {
+    Event::WindowEvent { event, .. } => match event {
+      WindowEvent::CloseRequested
+      | WindowEvent::KeyboardInput {
+        event:
+          KeyEvent {
+            state: ElementState::Pressed,
+            physical_key: PhysicalKey::Code(KeyCode::Escape),
+            ..
+          },
+        ..
+      } => {
+        target.exit();
+      }
+      _ => {}
+    },
+    _ => {}
+  })?;
+
+  Ok(())
 }
