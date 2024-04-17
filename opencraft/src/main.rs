0edfb3849e4e80c9e0e7b3ffd3d56532b1c7adf9
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-04-17 19:35:53 +0100

    Draw coloured rectangle

--- opencraft/src/main.rs
+++ opencraft/src/main.rs
@@ -77,16 +77,28 @@ struct Vertex {
 
 const VERTICES: &[Vertex] = &[
   Vertex {
-    position: [0.0, 0.5, 0.0],
+    position: [-0.8, 0.8, 0.0],
     color: [1.0, 0.0, 0.0],
   },
   Vertex {
-    position: [-0.5, -0.5, 0.0],
+    position: [-0.8, -0.8, 0.0],
     color: [0.0, 1.0, 0.0],
   },
   Vertex {
-    position: [0.5, -0.5, 0.0],
+    position: [0.8, 0.8, 0.0],
     color: [0.0, 0.0, 1.0],
+  },
+  Vertex {
+    position: [0.8, 0.8, 0.0],
+    color: [0.0, 0.0, 1.0],
+  },
+  Vertex {
+    position: [-0.8, -0.8, 0.0],
+    color: [0.0, 1.0, 0.0],
+  },
+  Vertex {
+    position: [0.8, -0.8, 0.0],
+    color: [1.0, 0.0, 0.0],
   },
 ];
 
