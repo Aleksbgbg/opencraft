6c72ef31ddb2dd90d5bd7bb24b894cb3595109dc
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-05-27 18:33:51 +0100

    camera: Extract rotor function out of impl block
    Since the rotor calculation does not directly operate on the camera
    itself, extract away from the impl block to keep it clear.

--- opencraft/src/camera.rs
+++ opencraft/src/camera.rs
@@ -4,6 +4,22 @@ use crate::core::math::rotor3::Rotor3;
 use crate::core::math::vec3::Vec3;
 use crate::core::math::{HALF_ROTATION, QUARTER_ROTATION, X_AXIS, Y_AXIS, Z_AXIS};
 
+fn rotor(rotation_x: Radians, rotation_y: Radians) -> Rotor3 {
+  let rotor_x = {
+    let orientation_x = Z_AXIS.angle_axis_rotate(rotation_x, X_AXIS);
+    Rotor3::new(Z_AXIS, orientation_x)
+  };
+  let rotor_y = if rotation_y == HALF_ROTATION {
+    let midpoint = Z_AXIS.angle_axis_rotate(QUARTER_ROTATION, Y_AXIS);
+    Rotor3::new(Z_AXIS, midpoint) * Rotor3::new(midpoint, -Z_AXIS)
+  } else {
+    let orientation_y = Z_AXIS.angle_axis_rotate(rotation_y, Y_AXIS);
+    Rotor3::new(Z_AXIS, orientation_y)
+  };
+
+  rotor_x * rotor_y
+}
+
 #[derive(Default)]
 pub struct Camera {
   position: Vec3,
@@ -16,24 +32,8 @@ impl Camera {
     Self::default()
   }
 
-  fn rotor(rotation_x: Radians, rotation_y: Radians) -> Rotor3 {
-    let rotor_x = {
-      let orientation_x = Z_AXIS.angle_axis_rotate(rotation_x, X_AXIS);
-      Rotor3::new(Z_AXIS, orientation_x)
-    };
-    let rotor_y = if rotation_y == HALF_ROTATION {
-      let midpoint = Z_AXIS.angle_axis_rotate(QUARTER_ROTATION, Y_AXIS);
-      Rotor3::new(Z_AXIS, midpoint) * Rotor3::new(midpoint, -Z_AXIS)
-    } else {
-      let orientation_y = Z_AXIS.angle_axis_rotate(rotation_y, Y_AXIS);
-      Rotor3::new(Z_AXIS, orientation_y)
-    };
-
-    rotor_x * rotor_y
-  }
-
   pub fn translate(&mut self, offset: Vec3) {
-    self.position += Self::rotor(self.rotation_x, self.rotation_y).rotate(offset);
+    self.position += rotor(self.rotation_x, self.rotation_y).rotate(offset);
   }
 
   pub fn rotate<X: Into<Radians>, Y: Into<Radians>>(&mut self, x: X, y: Y) {
@@ -54,6 +54,6 @@ impl Camera {
     } else {
       self.rotation_y
     };
-    mat4::rotate(-Self::rotor(self.rotation_x, rotation_y)) * mat4::translate(-self.position)
+    mat4::rotate(-rotor(self.rotation_x, rotation_y)) * mat4::translate(-self.position)
   }
 }
