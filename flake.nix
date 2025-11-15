26b3f2526638ddf1f9c5f35d103ac55e54a85ae3
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-11-15 18:34:23 +0000

    nix: Add Opencraft module
    Add a NixOS module which will build the static files necessary to run
    the game in the browser and make them available at `/var/run/opencraft`.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    self.submodules = true;
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,11 +11,14 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
     rust-overlay,
     ...
-  }:
+  }: let
+    packageName = "opencraft";
+  in (
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
@@ -22,13 +27,40 @@
         ];
       };
       lib = nixpkgs.lib;
+
+      libraryName = "l${packageName}";
+      buildTarget = "wasm32-unknown-unknown";
+
+      rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+        toolchain.minimal.override {
+          targets = [buildTarget];
+        });
+
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+
+      wasm-bindgen-cli = pkgs.buildWasmBindgenCli rec {
+        src = pkgs.fetchCrate {
+          pname = "wasm-bindgen-cli";
+          version = "0.2.105";
+          hash = "sha256-zLPFFgnqAWq5R2KkaTGAYqVQswfBEYm9x3OPjx8DJRY=";
+        };
+
+        cargoDeps = rustPlatform.fetchCargoVendor {
+          inherit src;
+          inherit (src) pname version;
+          hash = "sha256-a2X9bzwnMWNt0fTf30qAiJ4noal/ET1jEtf5fBFj5OU=";
+        };
+      };
     in {
       devShells.default = pkgs.mkShell rec {
         packages = with pkgs; [
           # Rust
           (rust-bin.selectLatestNightlyWith (toolchain:
             toolchain.complete.override {
-              targets = ["wasm32-unknown-unknown"];
+              targets = [buildTarget];
             }))
           cargo-watch # Continuous rebuild
           cargo-edit # Package updates
@@ -55,5 +87,70 @@
         ];
         LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
       };
-    });
+
+      packages.default = rustPlatform.buildRustPackage {
+        name = packageName;
+        version = "0.0.0";
+
+        src = lib.cleanSource ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = with pkgs; [
+          wasm-bindgen-cli
+          binaryen
+        ];
+
+        buildPhase = ''
+          runHook preBuild
+
+          cargo build --lib --release --target ${buildTarget}
+          wasm-bindgen target/${buildTarget}/release/${libraryName}.wasm --target web --out-dir dist
+          wasm-opt dist/${libraryName}_bg.wasm -O -o dist/${libraryName}_bg.wasm.opt
+
+          runHook postBuild
+        '';
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/assets
+
+          cp -r assets $out
+          cp dist/${libraryName}_bg.wasm.opt $out/${libraryName}_bg.wasm
+          cp dist/${libraryName}.js $out
+          cp ${packageName}/pkg/index.html $out
+
+          runHook postInstall
+        '';
+      };
+    })
+    // {
+      nixosModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+        with lib; let
+          description = "browser-runnable video game Opencraft";
+          cfg = config.services.opencraft;
+          runtimeFilesDir = "/var/run/${packageName}";
+        in {
+          options.services.opencraft.enable = mkEnableOption description;
+
+          config = mkIf cfg.enable {
+            users.groups."${packageName}" = {};
+            users.users."${packageName}" = {
+              group = "${packageName}";
+              isSystemUser = true;
+            };
+
+            systemd.tmpfiles.rules = [
+              # Type Path Mode User Group Age Argument
+              "d ${runtimeFilesDir} 0755 ${packageName} ${packageName} - -"
+              "L ${runtimeFilesDir}/frontend - - - - ${self.packages.${pkgs.system}.default}"
+            ];
+          };
+        };
+    }
+  );
 }
