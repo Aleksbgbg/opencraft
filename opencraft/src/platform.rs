162f190714374f7ad37b01dfd6d613070c04afce
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-11-16 11:10:08 +0000

    Switch from target_arch to target_family for wasm
    `target_family` is more accurate as it will allow us to compile for
    other wasm architectures as well.

--- opencraft/src/platform.rs
+++ opencraft/src/platform.rs
@@ -1,9 +1,9 @@
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(not(target_family = "wasm"))]
 mod desktop;
-#[cfg(target_arch = "wasm32")]
+#[cfg(target_family = "wasm")]
 mod web;
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(not(target_family = "wasm"))]
 pub use desktop::*;
-#[cfg(target_arch = "wasm32")]
+#[cfg(target_family = "wasm")]
 pub use web::*;
