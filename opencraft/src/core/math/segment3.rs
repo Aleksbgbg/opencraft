b18a60b389537168c7b502a1404af04ae6cfa576
Aleks Todorov <aleks.todorov.1337@gmail.com>
2025-12-01 22:08:26 +0000

    math: Convert Segment3 to origin-extent form
    Use origin-extent instead of start-len for Segment3 as this
    representation is more versatible for geometric operations which we will
    implement in upcoming changes.

--- opencraft/src/core/math/segment3.rs
+++ opencraft/src/core/math/segment3.rs
@@ -1,29 +1,44 @@
 use crate::core::math::vec3::Vec3;
 
 pub struct Segment3 {
-  start: Vec3,
+  origin: Vec3,
   direction: Vec3,
-  len: f32,
+  extent: f32,
 }
 
 impl Segment3 {
-  pub const fn start_direction_len(start: Vec3, direction: Vec3, len: f32) -> Self {
+  pub const fn origin_direction_extent(origin: Vec3, direction: Vec3, extent: f32) -> Self {
     Self {
-      start,
+      origin,
       direction,
-      len,
+      extent,
     }
+  }
+
+  pub fn start_direction_len(start: Vec3, direction: Vec3, len: f32) -> Self {
+    let extent = len / 2.0;
+    let origin = start + (direction * extent);
+
+    Self::origin_direction_extent(origin, direction, extent)
+  }
+
+  pub const fn origin(&self) -> Vec3 {
+    self.origin
   }
 
   pub const fn direction(&self) -> Vec3 {
     self.direction
   }
 
-  pub const fn start(&self) -> Vec3 {
-    self.start
+  pub const fn extent(&self) -> f32 {
+    self.extent
+  }
+
+  pub fn start(&self) -> Vec3 {
+    self.origin - (self.direction * self.extent)
   }
 
   pub fn end(&self) -> Vec3 {
-    self.start + (self.direction * self.len)
+    self.origin + (self.direction * self.extent)
   }
 }
